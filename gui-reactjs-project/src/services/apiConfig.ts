7fea3cc0e382f5a18fd7c9d6c81e8fe28be64a8d
Le Dang Khoa <103125520+ledangkhoa11-4@users.noreply.github.com>
2023-11-08 23:53:09 +0700

    check response api

--- gui-reactjs-project/src/services/apiConfig.ts
+++ gui-reactjs-project/src/services/apiConfig.ts
@@ -1,5 +1,7 @@
+import { routes } from "@/app/routers/routes";
 import { EAPI } from "@/models/general";
 import axios from "axios";
+import {redirect} from "next/navigation"
 
 const api = axios.create({
   baseURL: `${process.env.API_URL}`,
@@ -12,4 +14,14 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+api.interceptors.response.use((response) => {
+  return response;
+ }, (error) => {
+  if(error.response.status === 401 || error.response.status  === 403 ){
+    //refresh token
+    //Tham khảo tại: https://codestus.com/posts/refresh-token-trong-axios-nhu-the-nao
+  }
+  return Promise.reject(error);
+ });
+ 
 export default api;
