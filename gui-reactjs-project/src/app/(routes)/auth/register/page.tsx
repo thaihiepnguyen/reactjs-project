21f0da75c8b28c37d8d9e0e5eb2e93ae5855280c
PhongTran1804 <trannguyenphongg@gmail.com>
2023-11-09 21:28:36 +0700

    register form v1.1

--- gui-reactjs-project/src/app/(routes)/auth/register/page.tsx
+++ gui-reactjs-project/src/app/(routes)/auth/register/page.tsx
@@ -2,23 +2,19 @@
 // SignUpForm.jsx
 
 import React, { useState } from 'react';
-import { TextField, Button, Container, Typography } from '@mui/material';
+import { TextField, Button, Container, Typography, Radio, RadioGroup, FormControlLabel, FormControl } from '@mui/material';
 // import './SignUpFormStyles.scss';
 import { Helmet } from 'react-helmet';
 
 function RegisterForm() {
-  const [firstname, setFirstName] = useState('');
-  const [lastname, setLastName] = useState('');
+  const [fullname, setFullName] = useState('');
+  const [userType, setUserType] = useState('student'); // Default to 'student'
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleFirstNameChange = (event) => {
-    setFirstName(event.target.value);
-  };
-
-  const handleLastNameChange = (event) => {
-    setLastName(event.target.value);
+  const handleFullNameChange = (event) => {
+    setFullName(event.target.value);
   };
 
   const handleEmailChange = (event) => {
@@ -31,11 +27,29 @@ function RegisterForm() {
     validatePassword(newPassword);
   };
 
+  const handleUserTypeChange = (event) => {
+    setUserType(event.target.value);
+  };
+
+  // const checkUsernameAvailability = (value) => {
+  //   // Simulated asynchronous check for username availability
+  //   // You should replace this with actual API calls to your server
+  //   const existingUsernames = ['existinguser1', 'existinguser2']; // Example existing usernames
+
+  //   if (existingUsernames.includes(value)) {
+  //     setUsernameExists(true);
+  //     setError('This username is already taken. Please choose a different one.');
+  //   } else {
+  //     setUsernameExists(false);
+  //     setError('');
+  //   }
+  // };
+
   const validatePassword = (value) => {
     // Password validation logic
     const regex = /^(?=.*\d)(?=.*[A-Z]).{8,}$/;
     if (!regex.test(value)) {
-      setError('Password must be at least 8 characters, contain one number, and one uppercase letter.');
+      setError('Password must be at least 8 characters, contain at least 1 number, 1 uppercase letter.');
     } else {
       setError('');
     }
@@ -45,7 +59,7 @@ function RegisterForm() {
     event.preventDefault();
 
     // Additional form submission logic, e.g., sending data to the server
-    console.log('Form submitted:', { firstname, lastname, email, password });
+    console.log('Form submitted:', { fullname, email, password, userType });
   };
 
   return (
@@ -55,22 +69,13 @@ function RegisterForm() {
       </Typography>
       <form onSubmit={handleSubmit}>
         <TextField
-          label="First Name"
+          label="Full Name"
           variant="outlined"
           margin="normal"
           fullWidth
           required
-          value={firstname}
-          onChange={handleFirstNameChange}
-        />
-        <TextField
-          label="Last Name"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          required
-          value={lastname}
-          onChange={handleLastNameChange}
+          value={fullname}
+          onChange={handleFullNameChange}
         />
         <TextField
           type="email"
@@ -93,6 +98,16 @@ function RegisterForm() {
           onChange={handlePasswordChange}
         />
         {error && <Typography variant="body2" color="error">{error}</Typography>}
+        
+        <FormControl component="fieldset" margin="normal" fullWidth>
+          <Typography variant="subtitle1" gutterBottom>
+            You are:
+          </Typography>
+          <RadioGroup row value={userType} onChange={handleUserTypeChange}>
+            <FormControlLabel value="student" control={<Radio />} label="Student" />
+            <FormControlLabel value="teacher" control={<Radio />} label="Teacher" />
+          </RadioGroup>
+        </FormControl>
         <Button
           type="submit"
           variant="contained"
