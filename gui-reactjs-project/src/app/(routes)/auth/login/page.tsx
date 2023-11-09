b185713bf4a6be4dd98a1bd7c9bc90403d7a0eee
PhongTran1804 <trannguyenphongg@gmail.com>
2023-11-09 20:58:54 +0700

    register/sign up form v1.0

--- gui-reactjs-project/src/app/(routes)/auth/login/page.tsx
+++ gui-reactjs-project/src/app/(routes)/auth/login/page.tsx
@@ -1,9 +1,74 @@
 'use client'
 
 import { useAppSelector } from "@/redux/hook";
-import { Box, Button, Stack } from "@mui/material";
+import React, { useState } from 'react';
+import { TextField, Button, Container, Typography } from '@mui/material';
 import classes from "./styles.module.scss"
 import { signIn } from "next-auth/react";
+
+function SignUpForm() {
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+    validatePassword(newPassword);
+  };
+
+  const validatePassword = (value) => {
+    // Password validation logic
+    const regex = /^(?=.*\d)(?=.*[A-Z]).{8,}$/;
+    if (!regex.test(value)) {
+      setError('Password must be at least 8 characters, contain one number, and one uppercase letter.');
+    } else {
+      setError('');
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Additional form submission logic
+  };
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h4" align="center" gutterBottom>
+        Sign Up
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          type="email"
+          label="Email"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          required
+        />
+        <TextField
+          type="password"
+          label="Password"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          required
+          onChange={handlePasswordChange}
+        />
+        {error && <Typography variant="body2" color="error">{error}</Typography>}
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          disabled={Boolean(error)} 
+        >
+          Sign Up
+        </Button>
+      </form>
+    </Container>
+  );
+}
+
 
 export default function Login() {
   const {value} = useAppSelector(state => state.counterReducer)
@@ -12,7 +77,19 @@ export default function Login() {
     <>
       <span>Login page</span>
       <span>Counter: {value}</span>
-      <Button onClick={()=>signIn('google')}>Login with google</Button>
+      <SignUpForm />
+      <Container maxWidth="sm">
+        <Button 
+            className={classes.loginGoogleBtn}
+            type="submit"
+            variant="contained"
+            // color="white"
+            fullWidth
+            onClick={()=>signIn('google')}
+            >
+            Login with google
+        </Button>
+      </Container>
     </>
   )
 }
--- gui-reactjs-project/src/app/(routes)/auth/register/page.tsx
+++ gui-reactjs-project/src/app/(routes)/auth/register/page.tsx
@@ -1,6 +1,112 @@
 "use client";
-import { Button } from "@mui/material";
-import { Helmet } from "react-helmet";
+// SignUpForm.jsx
+
+import React, { useState } from 'react';
+import { TextField, Button, Container, Typography } from '@mui/material';
+// import './SignUpFormStyles.scss';
+import { Helmet } from 'react-helmet';
+
+function RegisterForm() {
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+    validatePassword(newPassword);
+  };
+
+  const validatePassword = (value) => {
+    // Password validation logic
+    const regex = /^(?=.*\d)(?=.*[A-Z]).{8,}$/;
+    if (!regex.test(value)) {
+      setError('Password must be at least 8 characters, contain one number, and one uppercase letter.');
+    } else {
+      setError('');
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Additional form submission logic, e.g., sending data to the server
+    console.log('Form submitted:', { firstname, lastname, email, password });
+  };
+
+  return (
+    <Container className="register-form" maxWidth="sm">
+      <Typography variant="h4" align="center" gutterBottom>
+        Register
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="First Name"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          required
+          value={firstname}
+          onChange={handleFirstNameChange}
+        />
+        <TextField
+          label="Last Name"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          required
+          value={lastname}
+          onChange={handleLastNameChange}
+        />
+        <TextField
+          type="email"
+          label="Email"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          required
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <TextField
+          type="password"
+          label="Password"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          required
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        {error && <Typography variant="body2" color="error">{error}</Typography>}
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          className="submit-button"
+          fullWidth
+          disabled={Boolean(error)}
+        >
+          Register
+        </Button>
+      </form>
+    </Container>
+  );
+}
 
 export default function Register() {
   return (
@@ -8,7 +114,8 @@ export default function Register() {
       <Helmet>
         <title>Register</title>
       </Helmet>
-      <span>Register Page</span>
+      {/* <span>Register Page</span> */}
+      <RegisterForm />
     </>
   );
 }
