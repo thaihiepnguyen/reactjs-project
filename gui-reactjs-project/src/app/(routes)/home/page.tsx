459ad157c43cc3aad2f0875d30dd7b531d9ba06c
PhongTran1804 <trannguyenphongg@gmail.com>
2023-11-15 12:54:27 +0700

    home banner initail

--- gui-reactjs-project/src/app/(routes)/home/page.tsx
+++ gui-reactjs-project/src/app/(routes)/home/page.tsx
@@ -1,10 +1,11 @@
-
+import HomeBanner from "@/app/components/HomeBanner"
 
 
 export default function Home() {
   return (
     <div style={{width: '100%', height: '100%', textAlign: "center"}}>
       Home page
+      <HomeBanner />
     </div>
   )
 }
---
+++ gui-reactjs-project/src/app/components/HomeBanner/index.tsx
@@ -0,0 +1,14 @@
+// components/HomeBanner.js
+import React from 'react';
+
+const HomeBanner = () => {
+  return (
+    <div className="banner-container">
+      {/* Add your banner content here */}
+      <h1>Welcome to Our Website</h1>
+      <p>Discover amazing products and services!</p>
+    </div>
+  );
+};
+
+export default HomeBanner;
