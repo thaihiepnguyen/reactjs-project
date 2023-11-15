a95ba52b9bdd7204cae99785d97ffc3772a371e6
PhongTran1804 <trannguyenphongg@gmail.com>
2023-11-15 13:07:32 +0700

    homebanner update

--- gui-reactjs-project/src/app/components/HomeBanner/index.tsx
+++ gui-reactjs-project/src/app/components/HomeBanner/index.tsx
@@ -1,10 +1,13 @@
 // components/HomeBanner.js
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import image from './homebanner.jpg'
 
 const HomeBanner = () => {
   return (
     <div className="banner-container">
       {/* Add your banner content here */}
+      <img src="./images/homebanner.jpg"/>
       <h1>Welcome to Our Website</h1>
       <p>Discover amazing products and services!</p>
     </div>
