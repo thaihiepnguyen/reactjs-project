25dfd322479cff71a72290a3aa5a73e601a56749
PhongTran1804 <trannguyenphongg@gmail.com>
2023-11-28 10:43:44 +0700

    add tab for mapping function of admin

--- gui-reactjs-project/src/app/(routes)/admin/mapping/page.tsx
+++ gui-reactjs-project/src/app/(routes)/admin/mapping/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, TextField } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, TextField, Tabs,
+  Tab} from "@mui/material";
+import { Input } from "@mui/icons-material";
 
 export default function MyNextJsExcelSheet() {
   const [items, setItems] = useState([]);
@@ -11,7 +13,7 @@ export default function MyNextJsExcelSheet() {
   const [manualMappingData, setManualMappingData] = useState([]);
   const [newStudentId, setNewStudentId] = useState("");
   const [newEmail, setNewEmail] = useState("");
-  const [showExcelTable, setShowExcelTable] = useState(false);
+  const [selectedTab, setSelectedTab] = useState(0);
 
   const readExcel = (file: Blob) => {
     const promise = new Promise((resolve, reject) => {
@@ -58,7 +60,6 @@ export default function MyNextJsExcelSheet() {
   }
 
   const handleManualMapping = () => {
-    setShowExcelTable(!showExcelTable);
     setShowManualMapping(!showManualMapping);
     setShowSaveButton(!showSaveButton);
     setManualMappingData([]); // Clear any previous manual mapping data
@@ -87,66 +88,86 @@ export default function MyNextJsExcelSheet() {
     }
   };
 
+  const handleTabChange = (event: any, newValue: React.SetStateAction<number>) => {
+    setSelectedTab(newValue);
+    setErrorMessage(""); // Clear error message when switching tabs
+  };
+
   return (
     <div>
-      <input
-        type="file"
-        onChange={(e) => {
-          const file = e.target.files[0];
-          readExcel(file);
-        }}
-      />
-      <br /><br /><br />
-      <Button variant="contained" color="primary" onClick={handleManualMapping}>
-        Mapping Manually
-      </Button>
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      {showSaveButton && (
-        <Button variant="contained" color="primary" onClick={handleSaveData}>
-          Save Data
-        </Button>
-      )}
-
-      {/* If admin chooses import excel file */}
-      {items.length > 0 && (
-        <TableContainer component={Paper}>
-          <Table aria-label="Excel Data Table">
-            <TableHead>
-              <TableRow>
-                {Object.keys(items[0]).map((header, index) => (
-                  <TableCell key={index}>{header}</TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {items.map((row, index) => (
-                <TableRow key={index}>
-                  {Object.values(row).map((value, i) => (
-                    <TableCell key={i}>{value}</TableCell>
-                  ))}
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      )}
-
-      {/* If admin chooses mapping manually */}
-      {showManualMapping && (
-        <div> 
-          <TextField
-            label="Student ID"
-            value={newStudentId}
-            onChange={(e) => setNewStudentId(e.target.value)}
+       <Tabs value={selectedTab} onChange={handleTabChange}>
+        <Tab label="Import Excel" />
+        <Tab label="Manual Mapping" />
+      </Tabs>
+      {selectedTab === 0 && (
+        <>
+          <input
+            type="file"
+            onChange={(e) => {
+              const file = e.target.files[0];
+              readExcel(file);
+            }} 
+            style={{
+              padding: '10px',
+              borderRadius: '5px',
+              backgroundColor: '#f9f9f9',
+              color: '#555',
+              transition: 'border-color 0.3s ease-in-out',
+              width: 'auto', // Set the width as needed
+            }}
           />
-          <TextField
-            label="Email"
-            value={newEmail}
-            onChange={(e) => setNewEmail(e.target.value)}
-          />
-          <Button variant="contained" color="primary" onClick={handlePushToTable}>
-            Push to Table
-          </Button>
+          <br /><br /><br />
+
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+          {showSaveButton && (
+            <Button variant="contained" color="primary" onClick={handleSaveData}>
+              Save Data
+            </Button>
+            )}
+          {/* If admin chooses import excel file */}
+          {items.length > 0 && (
+              <TableContainer component={Paper}>
+                <Table aria-label="Excel Data Table">
+                  <TableHead>
+                    <TableRow>
+                      {Object.keys(items[0]).map((header, index) => (
+                        <TableCell key={index}>{header}</TableCell>
+                       ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {items.map((row, index) => (
+                      <TableRow key={index}>
+                        {Object.values(row).map((value, i) => (
+                          <TableCell key={i}>{value}</TableCell>
+                        ))}
+                      </TableRow>
+                     ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            )}
+          </> 
+        )}
+      
+      {selectedTab === 1 && (
+        <>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '1rem', margin: '1rem 0' }}> 
+            <TextField
+              label="Student ID"
+              value={newStudentId}
+              onChange={(e) => setNewStudentId(e.target.value)}
+            />
+            <TextField
+              label="Email"
+              value={newEmail}
+              onChange={(e) => setNewEmail(e.target.value)}
+            />
+            <Button variant="contained" color="primary" onClick={handlePushToTable}>
+              Push to Table
+            </Button>
+          </div>
+          
           <TableContainer component={Paper}>
             <Table aria-label="Manual Mapping Table">
               <TableHead>
@@ -175,7 +196,8 @@ export default function MyNextJsExcelSheet() {
               </TableBody>
             </Table>
           </TableContainer>
-        </div>)}
+        </>
+      )}
     </div>
   );
 }
