31a81bebaf48f28ee704d35bf2decf37f2d03e7f
PhongTran1804 <trannguyenphongg@gmail.com>
2023-11-30 17:15:02 +0700

    fix bug

--- gui-reactjs-project/src/app/page.tsx
+++ gui-reactjs-project/src/app/page.tsx
@@ -1,9 +1,7 @@
 import LandingPage from "./components/LandingPage";
-import DetailCourse from "./(routes)/user/my-courses/detail-course/page";
 export default function Page() {
 
   return (
-      <DetailCourse />
-      // <LandingPage/>
+      <LandingPage/>
   );
 }
