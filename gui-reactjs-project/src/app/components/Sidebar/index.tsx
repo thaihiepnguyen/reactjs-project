0e0c028f7c472a479689e045879cc04648401085
hiepnt <thaihiep232002@gmail.com>
2023-12-01 17:03:31 +0700

    fix sidebar

--- gui-reactjs-project/src/app/components/Sidebar/index.tsx
+++ gui-reactjs-project/src/app/components/Sidebar/index.tsx
@@ -4,8 +4,9 @@ import classes from "./styles.module.scss";
 import Link from "next/link";
 import { routes } from "@/app/routers/routes";
 import {HomeOutlined, SettingsOutlined,NotificationsNoneOutlined, ArrowBackIosOutlined, ArrowForwardIosOutlined, AccountCircleOutlined, SchoolOutlined, MenuBookOutlined} from '@mui/icons-material';
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import classNames from 'classnames';
+import { usePathname } from 'next/navigation'
 
 const sidebarItems = [
   {
@@ -42,12 +43,15 @@ const sidebarItems = [
 
 export default function Sidebar() {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
-  const [active, setActive] = useState<number>(sidebarItems.reduce((acc, cur, index) => {
-    if (window.location.pathname == cur.href) acc = index
-    return acc
-  }, 0));
+  const pathname = usePathname();
+  const [active, setActive] = useState<number>(0);
   const toggle = () => setSidebarOpen((prev) => !prev);
-
+  useEffect(() => {
+    setActive(sidebarItems.reduce((acc, cur, index) => {
+      if (pathname == cur.href) acc = index
+      return acc
+    }, 0))
+  }, [pathname])
   return <>
     <div className={classes.sidebarWrapper}>
       <button className={classes.sidebarButton} onClick={toggle}>{ sidebarOpen ? <ArrowBackIosOutlined/> : <ArrowForwardIosOutlined/>}</button>
